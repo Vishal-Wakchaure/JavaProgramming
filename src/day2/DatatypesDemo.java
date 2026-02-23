caaeb0227e8b3c68274c07e1fa4f1d39a608cc85
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-23 19:16:20 +0530

    Java programming for Automation testing

---
+++ src/day2/DatatypesDemo.java
@@ -0,0 +1,45 @@
+package day2;
+
+public class DatatypesDemo {
+
+	public static void main(String[] args) {
+		
+		//Numeric data types 
+		int a=200, b=200;
+		System.out.println("The value of a is "+a);
+		System.out.println("The value of b is " +b );
+		System.out.println("The sum is " + (a+b));
+		
+		byte y = 125;
+		System.out.println(y);
+		
+		short s = 32;
+		System.out.println(s);
+		
+		long l = 12345677889l;
+		System.out.println(l);
+		
+		float f = 5367737783.8282f;
+		System.out.println(f);
+		
+		//Decimal Numbers -> float, double 
+		float item = 7.29f;
+		System.out.println(item);
+		
+		char grade = 'A';
+		System.out.println(grade);
+		
+		String name = "Vishal";
+		System.out.println(name);
+		
+		boolean bl = false;//allows only true/false
+		System.out.println(bl);
+		
+		String b1= "true";
+		System.out.println(b1);
+
+		
+
+	}
+
+}
---
+++ src/day2/VariablesDemo.java
@@ -0,0 +1,39 @@
+package day2;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		
+//		int a;  //Declaration 
+//		a=100; //Assignment 
+		
+//		int a=100; //Declaration + Assignment
+//		System.out.println(a);
+
+//      Variable declaration Approaches 
+		/*Approach1 - if all variables are belong to same datatypes 
+		int a = 100;
+		int b =200;
+		int c=300;*/
+		
+		/* Approach2
+		 * int a,b,c;
+		 * a=100;
+		 * b=200;
+		 * c=300;
+		 */
+		
+//		Approach3 
+		  int a=100,b=200,c=300;
+		  System.out.println("The value of a is : "+a);
+		  System.out.println("The value of b is :"+b);
+		  System.out.println("The value of c is :"+c);
+		  
+		  System.out.println(a + " "+b + " " +c );
+		
+		 
+		
+		
+	}
+
+}
