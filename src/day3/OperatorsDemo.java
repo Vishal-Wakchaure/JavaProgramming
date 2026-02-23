bcef45381e69a8d69b514ba845a59858bfba6d5d
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-23 20:01:30 +0530

    Java programming for Automation testing

---
+++ src/day3/OperatorsDemo.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) 
+	{
+		
+		int a=5,b=2;
+		System.out.println("Sum of two numbers is " + (a+b));
+		System.out.println("Sub of two numbers is " + (a-b));
+		System.out.println("Mul of two numbers is " + (a*b));
+		System.out.println("Div of two numbers is " + (a/b));
+		System.out.println("mod  of two numbers is " + (a%b));
+		
+
+	}
+
+}
