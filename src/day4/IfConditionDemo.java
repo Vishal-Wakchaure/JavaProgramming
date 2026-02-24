5e98ae788804467d1e93fde5e7dcc47ffa26c064
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-24 13:26:59 +0530

    Java programming for Automation testing

---
+++ src/day4/IfConditionDemo.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class IfConditionDemo {
+
+	public static void main(String[] args) {
+		
+		int age = 25;
+		
+		if(age>=18)
+			System.out.println("Person is eligible for voting");
+		else
+		{
+			System.out.println("Not eligible for vote");
+		}
+	
+
+	}
+
+}
---
+++ src/day4/IfElseDemo.java
@@ -0,0 +1,117 @@
+package day4;
+
+public class IfElseDemo {
+	public static void main(String[] args) {
+		
+//		int age =17;
+//		if (age>=18)
+//			System.out.println("Eligible for vote");
+//		else 
+//		{
+//			System.out.println("Not eligible for vote");
+//		}
+		
+//		Check number is odd or even
+//		int a=10;
+//		if(a%2==0)
+//		System.out.println("Number is even");
+//		else 
+//			System.out.println("Number is odd");
+		
+////		Ex.Number is positive,negative or zero
+//		int a= 0;
+//		if (a>0)
+//		{
+//			System.out.println("Number is positive");
+//		}
+//		else if(a<0)
+//		{
+//			System.out.println("Number is negative");
+//		}
+//		else
+//		{
+//			System.out.println("number equal to zero");
+//		}
+//		Largest Among three numbers
+//		int a=100,b=200,c=30;
+//				if(a>b && a>c)
+//				{
+//					System.out.println(a + " is greater");
+//				}
+//				else if(b>c && b>a)
+//				{
+//					System.out.println(b + " is greater");
+//				}
+//				else
+//				{
+//					System.out.println(c + " is greater");
+//				}
+		
+//		if(true)
+//		{
+//			System.out.println(1);
+//		}
+//		else 
+//		{
+//			System.out.println(2);
+//		}
+		
+//		if(false)
+//		{
+//			if(false)
+//			{
+//				System.out.println("abc");
+//			}
+//			else
+//			{
+//				System.out.println("xyz");
+//			}
+//		}
+//		else 
+//		{
+//			System.out.println("123");
+//		}
+		
+		int weekno =5;
+		if(weekno==1)
+		{
+			System.out.println("sunday");
+		}
+		else if (weekno==2)
+		{
+			System.out.println("monday");
+		}
+		else if (weekno==3)
+		{
+			System.out.println("Tuesday");
+		}
+		else if (weekno==4)
+		{
+			System.out.println("Wednesday");
+		}
+		else if (weekno==5)
+		{
+			System.out.println("Thursday");
+		}
+		else if (weekno==6)
+		{
+			System.out.println("Friday");
+		}
+		else if (weekno==7)
+		{
+			System.out.println("Saturday");
+		}
+		else
+		{
+			System.out.println("invalid day number");
+		}
+	
+	   
+	    
+	    	
+	
+	}
+	
+	}
+
+
