57eec41da9279aa6c6f5b3452aec4e4fd864059d
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-04-02 23:14:26 +0530

    Java programs

---
+++ src/tcsnqt/VowelsCount.java
@@ -0,0 +1,21 @@
+package tcsnqt;
+
+public class VowelsCount {
+	
+	public static void main(String[] args) {
+		
+		String str = "wakchaure";
+		int count =0;
+		
+		for(int i=0;i<str.length();i++)
+		{
+			char ch = str.charAt(i);
+		
+		if(ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u' )
+			count++;
+		}
+		System.out.println("vowels" +count);
+		
+	}
+
+}
