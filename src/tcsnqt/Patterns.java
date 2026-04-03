0d1475ea1d17ddb333ad96f224f7ac55fbfee659
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-04-03 22:55:35 +0530

    Java programs

--- src/tcsnqt/Patterns.java
+++ src/tcsnqt/Patterns.java
@@ -17,13 +17,13 @@ public class Patterns {
 //			System.out.println();
 //		}
 		
-		for(int i=1;i<=4;i++) {
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
+//		for(int i=1;i<=4;i++) {
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print("* ");
+//			}
+//			System.out.println();
+//		}
 		
 	}
 
